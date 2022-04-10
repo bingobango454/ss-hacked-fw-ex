a32c630a84b8ca8ddf42cb39174f2052a6b3b510
hk <hk@ryan.com>
2022-04-10 05:07:11 +0000

    init hacked info/files softswiss

---
+++ profile rule scripts/porteus.sh
@@ -0,0 +1,6 @@
+export DISTRO=porteus
+export BOOTDEV=/mnt/sda
+export BASEDIR=/mnt/sda
+export PORTDIR=/mnt/sda/porteus
+export MODDIR=/mnt/sda/porteus/modules
+export PORTCFG=/mnt/sda/porteus/porteus-v5.0-x86_64.cfg
